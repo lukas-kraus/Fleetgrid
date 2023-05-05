160305ea094994dc2d0f6f3a93bb84a02783f905
Lukas <lukaskraus@me.com>
2023-05-05 11:50:17 +0200

    Simple login page frontend

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,32 @@
+import {FormEvent, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import './AddCar.css';
+
+type Props = {
+    onLogin: (username: string, password: string) => Promise<void>
+}
+
+export default function Login(props: Props) {
+
+    const [username, setUsername] = useState<string>("")
+    const [password, setPassword] = useState<string>("")
+    const navigate = useNavigate();
+
+    function onSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+
+        props.onLogin(username, password)
+            .then(() => {
+                navigate("/cars")
+            })
+
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <input value={username} placeholder="username" type="text" onChange={e => setUsername(e.target.value)}/>
+            <input value={password} placeholder="password" type="password" onChange={e => setPassword(e.target.value)}/>
+            <button type="submit">Login</button>
+        </form>
+    )
+}
