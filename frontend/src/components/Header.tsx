2d8ed860d81fe0f7aa8981035e819ac309ad3aef
Lukas <lukaskraus@me.com>
2023-05-05 18:26:03 +0200

    Navigation bar

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,22 @@
 import logo from '../logo.png'
 import './Header.css';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import {NavLink} from "react-router-dom";
 
 export default function Header() {
     return (
         <header>
             <NavLink to="/"><img src={logo} className="logo" alt="Fleetlink"/></NavLink>
-            <NavLink to="/cars">All Cars</NavLink>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/cars">Cars</NavLink>
+            <NavLink to="#">Trips</NavLink>
+            <NavLink to="#">Driver</NavLink>
+            <NavLink to="#">Settings</NavLink>
+            <div className="user">
+                <NavLink to="#">
+                    <AccountCircleIcon/> <span>Lukas</span>
+                </NavLink>
+            </div>
         </header>
     )
 }
