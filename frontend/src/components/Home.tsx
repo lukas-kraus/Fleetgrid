2717d9dba291acabc382ec5360f9141cb4f7daec
Lukas <lukaskraus@me.com>
2023-05-11 11:42:05 +0200

    Implemented stats

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,11 +1,62 @@
+import {Car} from "../model/Car";
+import './Home.css';
+import StreetviewIcon from '@mui/icons-material/Streetview';
+import LocalParkingIcon from '@mui/icons-material/LocalParking';
+import EvStationIcon from '@mui/icons-material/EvStation';
+import TimeToLeaveIcon from '@mui/icons-material/TimeToLeave';
+
 type Props = {
     user: string | undefined
+    cars: Car[]
 }
 
 export default function Home(props: Props) {
+
+    function countCars(status: string): number {
+        return status ? props.cars.filter((car) => car.status.toLowerCase() === status.toLowerCase()).length : props.cars.length;
+    }
+
     return (
-        <div>
+        <>
             <h1>Hi {props.user}!</h1>
-        </div>
+            <div className="stats">
+                <div className="stat">
+                    <div className="icon">
+                        <StreetviewIcon/>
+                    </div>
+                    <div className="text">
+                        <h2>On the way</h2>
+                        <span className="number">{countCars("OTW")}</span>
+                    </div>
+                </div>
+                <div className="stat">
+                    <div className="icon">
+                        <LocalParkingIcon/>
+                    </div>
+                    <div className="text">
+                        <h2>Parked</h2>
+                        <span className="number">{countCars("Parked")}</span>
+                    </div>
+                </div>
+                <div className="stat">
+                    <div className="icon">
+                        <EvStationIcon/>
+                    </div>
+                    <div className="text">
+                        <h2>Charging</h2>
+                        <span className="number">{countCars("Charging")}</span>
+                    </div>
+                </div>
+                <div className="stat">
+                    <div className="icon">
+                        <TimeToLeaveIcon/>
+                    </div>
+                    <div className="text">
+                        <h2>In Total</h2>
+                        <span className="number">{countCars("")}</span>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
