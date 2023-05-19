808ba0d8ff4cd89d32c277154e04cf2cc2f83ab9
Lukas <lukaskraus@me.com>
2023-05-19 16:44:25 +0200

    Filter cars in frontend

--- frontend/src/components/car/CarGallery.tsx
+++ frontend/src/components/car/CarGallery.tsx
@@ -1,19 +1,45 @@
 import {Car} from "../../model/Car";
-import {NavLink} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 import CarCard from "./CarCard";
 import './CarGallery.css';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
+import React, {useState} from "react";
+import StreetviewIcon from "@mui/icons-material/Streetview";
+import LocalParkingIcon from "@mui/icons-material/LocalParking";
+import EvStationIcon from "@mui/icons-material/EvStation";
 
 type Props = {
     cars: Car[]
 }
 
 export default function CarGallery(props: Props) {
+    const [filterStatus, setFilterStatus] = useState('all');
+
+    const handleFilterClick = (status: string) => {
+        setFilterStatus(status);
+    };
     return (
         <>
             <h1>Cars</h1>
             <div className="right">
                 <NavLink to="/cars/add"><AddCircleIcon/></NavLink>
+            </div>
+            <div className="filter">
+                <Link to="#" className="chip no_icon" onClick={() => handleFilterClick('all')}>
+                    <span className="label">All</span>
+                </Link>
+                <Link to="#" className="chip" onClick={() => handleFilterClick('OTW')}>
+                    <span className="icon"><StreetviewIcon/></span>
+                    <span className="label">On the way</span>
+                </Link>
+                <Link to="#" className="chip" onClick={() => handleFilterClick('CHARGING')}>
+                    <span className="icon"><EvStationIcon/></span>
+                    <span className="label">Charging</span>
+                </Link>
+                <Link to="#" className="chip" onClick={() => handleFilterClick('PARKED')}>
+                    <span className="icon"><LocalParkingIcon/></span>
+                    <span className="label">Parked</span>
+                </Link>
             </div>
             <table>
                 <thead>
@@ -25,7 +51,9 @@ export default function CarGallery(props: Props) {
                 </tr>
                 </thead>
                 {
-                    props.cars.map((car) => <CarCard key={car.id} car={car}/>)
+                    props.cars
+                        .filter(car => filterStatus === 'all' || car.status === filterStatus)
+                        .map(car => <CarCard key={car.id} car={car}/>)
                 }
             </table>
         </>
