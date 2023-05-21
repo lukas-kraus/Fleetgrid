a3478e9bd8d5f5054046b3ba3b9ba4e53c323615
Lukas <lukaskraus@me.com>
2023-05-21 21:57:15 +0200

    Change table style

--- frontend/src/components/car/CarCard.tsx
+++ frontend/src/components/car/CarCard.tsx
@@ -6,13 +6,21 @@ type Props = {
 }
 
 export default function CarCard(props: Props) {
+
+    const statusAbbreviation: Record<string, string> = {
+        'OTW': 'On the way',
+        'PARKED': 'Parked',
+        'CHARGING': 'Charging'
+    };
+    const status: string = statusAbbreviation[props.car.status] || props.car.status;
+
     return (
         <tbody>
         <tr>
             <td><Link to={props.car.id}>{props.car.license_plate}</Link></td>
             <td>{props.car.model}</td>
             <td>{props.car.color}</td>
-            <td>{props.car.status}</td>
+            <td className="status"><span className={props.car.status.toLowerCase()}>{status}</span></td>
         </tr>
         </tbody>
     )
