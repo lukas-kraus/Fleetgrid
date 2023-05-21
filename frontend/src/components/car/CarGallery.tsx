7f0e0b94f7f3822d2054361242568b6f250f3152
Lukas <lukaskraus@me.com>
2023-05-21 16:54:01 +0200

    Highlight selected filters

--- frontend/src/components/car/CarGallery.tsx
+++ frontend/src/components/car/CarGallery.tsx
@@ -1,61 +1,105 @@
 import {Car} from "../../model/Car";
-import {Link, NavLink} from "react-router-dom";
+import {Link, NavLink, useSearchParams} from "react-router-dom";
 import CarCard from "./CarCard";
-import './CarGallery.css';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import React, {useState} from "react";
+import "./CarGallery.css";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import React, {useEffect, useState} from "react";
 import StreetviewIcon from "@mui/icons-material/Streetview";
 import LocalParkingIcon from "@mui/icons-material/LocalParking";
 import EvStationIcon from "@mui/icons-material/EvStation";
 
 type Props = {
-    cars: Car[]
-}
-
+    cars: Car[];
+};
 export default function CarGallery(props: Props) {
-    const [filterStatus, setFilterStatus] = useState('all');
-
-    const handleFilterClick = (status: string) => {
-        setFilterStatus(status);
-    };
-    return (
-        <>
-            <h1>Cars</h1>
-            <div className="right">
-                <NavLink to="/cars/add"><AddCircleIcon/></NavLink>
-            </div>
-            <div className="filter">
-                <Link to="#" className="chip no_icon" onClick={() => handleFilterClick('all')}>
-                    <span className="label">All</span>
-                </Link>
-                <Link to="#" className="chip" onClick={() => handleFilterClick('OTW')}>
-                    <span className="icon"><StreetviewIcon/></span>
-                    <span className="label">On the way</span>
-                </Link>
-                <Link to="#" className="chip" onClick={() => handleFilterClick('CHARGING')}>
-                    <span className="icon"><EvStationIcon/></span>
-                    <span className="label">Charging</span>
-                </Link>
-                <Link to="#" className="chip" onClick={() => handleFilterClick('PARKED')}>
-                    <span className="icon"><LocalParkingIcon/></span>
-                    <span className="label">Parked</span>
-                </Link>
-            </div>
-            <table>
-                <thead>
-                <tr>
-                    <th>License plate</th>
-                    <th>Model</th>
-                    <th>Color</th>
-                    <th>Status</th>
-                </tr>
-                </thead>
-                {
-                    props.cars
-                        .filter(car => filterStatus === 'all' || car.status === filterStatus)
-                        .map(car => <CarCard key={car.id} car={car}/>)
-                }
-            </table>
-        </>
-    )
+    const [searchParams] = useSearchParams();
+    const currentStatus = searchParams.get("status") || "all";
+    const [filterStatus, setFilterStatus] = useState(currentStatus);
+    useEffect(() => {
+        setFilterStatus(currentStatus);
+        console.log("Status: " + filterStatus)
+        // eslint-disable-next-line
+    }, [currentStatus]);
+    return (<>
+        <h1>Cars</h1>
+        <div className="right">
+            <NavLink to="/cars/add">
+                <AddCircleIcon/>
+            </NavLink>
+        </div>
+        <div className="filter">
+            <Link to=""
+                  className={
+                      `no_icon chip ${
+                          filterStatus === "all"
+                              ? "active"
+                              : ""
+                      }`
+                  }
+                  key="">
+                <span className="label">All</span>
+            </Link>
+            <Link to="?status=otw"
+                  className={
+                      `chip ${
+                          filterStatus === "otw"
+                              ? "active"
+                              : ""
+                      }`
+                  }
+                  key="otw">
+                <span className="icon">
+                    <StreetviewIcon/>
+                </span>
+                <span className="label">On the way</span>
+            </Link>
+            <Link to="?status=charging"
+                  className={
+                      `chip ${
+                          filterStatus === "charging"
+                              ? "active"
+                              : ""
+                      }`
+                  }
+                  key="charging">
+                <span className="icon">
+                    <EvStationIcon/>
+                </span>
+                <span className="label">Charging</span>
+            </Link>
+            <Link to="?status=parked"
+                  className={
+                      `chip ${
+                          filterStatus === "parked"
+                              ? "active"
+                              : ""
+                      }`
+                  }
+                  key="parked">
+                <span className="icon">
+                    <LocalParkingIcon/>
+                </span>
+                <span className="label">Parked</span>
+            </Link>
+        </div>
+        <table>
+            <thead>
+            <tr>
+                <th>License plate</th>
+                <th>Model</th>
+                <th>Color</th>
+                <th>Status</th>
+            </tr>
+            </thead>
+            {
+                props
+                    .cars
+                    .filter((car) => filterStatus === "all" || car.status.toLowerCase() === filterStatus.toLowerCase())
+                    .map((car) => (<CarCard key={
+                        car.id
+                    }
+                                            car={car}/>))
+            }
+        </table>
+    </>);
 }
